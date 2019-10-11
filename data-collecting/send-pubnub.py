a8c96f40144fd254930dac7dbf1193dc6c594efc
lythi45 <scala.bine@gmail>
2019-10-11 22:07:06 +0000

    modified send-pubnub

--- data-collecting/send-pubnub.py
+++ data-collecting/send-pubnub.py
@@ -7,8 +7,8 @@ from pubnub.exceptions import PubNubException
 import Adafruit_DHT as dht
 import configparser
 
-#import logging
-#logging.basicConfig(filename='/home/pi/logs/weather/temperature.log', filemode='a', format='%(created)f %(message)s', level=logging.INFO) 
+import logging
+logging.basicConfig(filename='/home/pi/logs/weather/temperature.log', filemode='a', format='%(created)f %(message)s', level=logging.INFO) 
 
 
 config = configparser.ConfigParser()
@@ -21,6 +21,9 @@ pnconfig.publish_key = config['DEFAULT']['publish_key']
 
 pubnub = PubNub(pnconfig)
 
+#print("Sub",pnconfig.subscribe_key)
+#print("Pub",pnconfig.publish_key)
+
 pubnub.subscribe().channels('tempeon').execute()
 pubnub.subscribe().channels('humeon').execute()
 
@@ -29,7 +32,8 @@ def callback(message):
 
 while True:
     try:
-        h,t = dht.read_retry(dht.DHT11, 4)
+        h,t = dht.read_retry(dht.DHT11, 23)
+        #print(h,t)
         envelope = pubnub.publish().channel('tempeon').message({
                 'x': time.time(),
                 'temperature_celsius': t}).sync()
@@ -40,7 +44,7 @@ while True:
             }).sync()
         print("publish timetoken: %d" % envelope.result.timetoken)
         #logging
-        #logging.info('Temp={0:0.1f} C and Humidity={1:0.1f} %'.format(t, h)) 
+        logging.info('Temp={0:0.1f} C and Humidity={1:0.1f} %'.format(t, h)) 
         #wait 5 mins
         time.sleep(300)
     except PubNubException as e:
